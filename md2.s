8040a7fe81bc46220254fb1579afb5d0c090a493
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-05 23:19:01 +0200

    commit

--- md2.s
+++ md2.s
@@ -4,15 +4,17 @@
 .type matmul, %function
 
 matmul:
-  sub sp, sp, #12
-  stmia sp!, {lr}
+@ str lr, [sp, #-4]!
+  mv lr, r4
+  sub sp, sp, #8
   stmia sp!, {r0, r1}
   ldr r0, f__a
 @ from stack pointer get first arg
-  ldmfd sp!, {r1, r2}
-  bl printf
-  ldmfd sp!, {lr}
+@ ldmfd sp!, {r1, r2}
+@ bl printf
+@ ldmfd sp!, {lr}
 // return
+  mv r4, lr
   bx lr
 @ h__a: .word height1
 @ w__a: .word width1
