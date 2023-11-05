3ef9cdf0445ead068866cc304162926970a7c849
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-05 22:42:48 +0200

    commit

--- md2_main.c
+++ md2_main.c
@@ -4,24 +4,52 @@
 
 int main(int argc, char *argv[])
 {
-  int w, h;
-  int *matrix;
+  int w1, h1, w2, h2;
+  int *matrix1;
+  int *matrix2;
+  int *matrix_result;
   int *p;
 
-  scanf("%d %d", &h, &w);
+  scanf("%d %d", &w1, &h1);
 
   // allocate memory for matrix
-  matrix = (int *)malloc(sizeof(int) * h * w);
+  matrix1 = (int *)malloc(sizeof(int) * h1 * w1);
 
   // read matrix
-  for (p = matrix; p < matrix + h * w; p++)
+  for (p = matrix1; p < matrix1 + h1 * w1; p++)
     scanf("%d", p);
 
+  scanf("%d %d", &w2, &h2);
+
+  // allocate memory for matrix
+  matrix2 = (int *)malloc(sizeof(int) * h2 * w2);
+
+  // read matrix
+  for (p = matrix2; p < matrix2 + h2 * w2; p++)
+    scanf("%d", p);
+
+  int w_result = w1;
+  int h_result = h2;
+  // allocate memory for matrix
+  matrix_result = (int *)malloc(sizeof(int) * h_result * w_result);
+
   // print matrix
-  printf("W:%d H:%d", w, h);
+  printf("W1:%d H1:%d", w1, h1);
+  for (p = matrix1; p < matrix1 + h1 * w1; p++)
+    printf("%d ", *p);
+  printf("\n");
+
+  printf("W2:%d H2:%d", w2, h2);
+  for (p = matrix2; p < matrix2 + h2 * w2; p++)
+    printf("%d ", *p);
+  printf("\n");
+
+  // call matmul
 
   // free memory
-  free(matrix);
+  free(matrix1);
+  free(matrix2);
+  free(matrix_result);
 
   return 0;
 }
