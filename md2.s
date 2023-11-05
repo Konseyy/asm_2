7e971fc3aae30baa72d153f9491c23bf6146f1a0
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-05 23:48:20 +0200

    c

--- md2.s
+++ md2.s
@@ -4,30 +4,31 @@
 .type matmul, %function
 
 matmul:
-  str lr, [sp, #-4]!
+  str lr, [sp, #-4]! @ Stack: sp -> lr
 @ sub sp, sp, #8
 @ stmia sp!, {r0, r1}
-  str r1, [sp, #-4]! @ Stack: sp -> r1
-  str r0, [sp, #-4]! @ Stack: sp -> r0, r1
+  str r1, [sp, #-4]! @ Stack: sp -> r1, lr
+  str r0, [sp, #-4]! @ Stack: sp -> r0, r1, lr
 
-  ldr r0, f__a @ load format string into r0
-  ldr r1, [sp, #0]@ load original r1 into r1
-@ Stack: r0
+  ldr r0, f__1a @ load format string into r0
+  ldr r1, [sp, #0]@ load original ro (first arg) into r1
+@ Stack: sp -> r0, r1, lr
   bl printf
 
-  ldr r0, f__a @ load format string into r0
-  ldr r1, [sp, #4]! @ load original r0 into r1
-@ Stack: -
+  ldr r0, f__2a @ load format string into r0
+  ldr r1, [sp, #4]! @ load original r1 (second arg) into r1
+@ Stack: sp -> r1, lr
   bl printf
 
   ldr lr, [sp, #4]! @ load original r0 into r1
-@ ldrfd sp!, {r1}
-@ bl printf
-@ ldmfd sp!, {lr}
-// return
+@ Stack: sp -> lr
+  add sp, sp, #4 @ Stack: sp ->
+@ return
   bx lr
 @ h__a: .word height1
 @ w__a: .word width1
-f__a:   .word format
+f__1a:    .word format1
+f__2a:    .word format2
 .data
-format: .asciz "from assembly, %d\n"
+format1:  .asciz "H: %d\n"
+format2:  .asciz "W: %d\n"
