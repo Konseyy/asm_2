2e7027ed5ccd40d8f9ebdcea323bb564489551d1
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-05 22:38:17 +0200

    commit

--- md2_main.c
+++ md2_main.c
@@ -4,11 +4,24 @@
 
 int main(int argc, char *argv[])
 {
-  char input[100];          // Declare a char array to hold the input
-  fgets(input, 100, stdin); // Read up to 100 characters from stdin
+  int w, h;
+  int *matrix;
+  int *p;
 
-  // print to stdout
-  printf("Hello, %s\n", input);
+  scanf("%d %d", &h, &w);
+
+  // allocate memory for matrix
+  matrix = (int *)malloc(sizeof(int) * h * w);
+
+  // read matrix
+  for (p = matrix; p < matrix + h * w; p++)
+    scanf("%d", p);
+
+  // print matrix
+  printf("W:%d H:%d", w, h);
+
+  // free memory
+  free(matrix);
 
   return 0;
 }
