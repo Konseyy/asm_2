91ba4832f278093053f9d2b43accd7ba38029f2f
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-07 00:47:58 +0200

    commit

--- md2.s
+++ md2.s
@@ -9,14 +9,10 @@ matmul:
 @ sp -> r0, r1, r2, r3, lr
 @ # 0 = h1, #4 = w1, #8 = *m1, #12 = h2, #16 = LR, #20 = w2, #24 = *m2, #28 = *m3
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-  ldr r6, [sp, #8] @ load *m1 into r5
-  ldr r0, f__1a
-  mov r1, r6
-  bl printf
-  @ ldr r5, =r6
-  ldr r0, [r5], #0 @ load m1[0] into r0
+  ldr r5, [sp, #8]@ load *m1 into r5
+@ ldr r0, [r5, #0] @ load m1[0] into r0
   ldr r1, [sp, #0]
-  str r1, [r5], #0
+  str r1, [r5, #0]
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   ldr lr, [sp, #16]@ load original lr into lr
 @ Stack: sp -> lr
