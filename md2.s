591c9996788960224ab5f3e696ad286dd2388c58
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-06 23:48:24 +0200

    commit

--- md2.s
+++ md2.s
@@ -6,12 +6,8 @@
 @ int matmul(int h1, int w1, int *m1, int h2, int w2, int *m2, int *m3);
 matmul:
   stmfd sp!, {r0, r1, r2, r3, lr}
-@ sp -> lr, h1, w1, *m1, h2, w2, *m2, *m3
-@ #0 = lr, #4 = h1, #8 = w1, #12 = *m1, #16 = h2, #20 = w2, #24 = *m2, #28 = *m3
-
-  ldr r0, f__2a
-  mov r1, lr
-  bl printf
+@ sp -> r0, r1, r2, r3, lr
+@ #0 = h1, #4 = w1, #8 = *m1, #12 = h2, #16 = LR, #20 = w2, #24 = *m2, #28 = *m3
 
   ldr r0, f__1a @ load format string into r0
   ldr r1, [sp, #0]@ load original ro (first arg) into r1
@@ -47,15 +43,7 @@ matmul:
   ldr r1, [sp, #28]
   bl printf
 
-  ldr r0, f__1a
-  ldr r1, [sp, #32]
-  bl printf
-
-  ldr r0, f__2a
-  ldr r1, [sp, #36]
-  bl printf
-
-  ldr lr, [sp, #-4]@ load original r0 into r1
+  ldr lr, [sp, #16]@ load original r0 into r1
 @ Stack: sp -> lr
   add sp, sp, #28 @ Stack: sp ->
 @ return
