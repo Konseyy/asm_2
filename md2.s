cec9f47a83ff64329755a2f66929f64b7a60ab03
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-07 01:14:19 +0200

    c

--- md2.s
+++ md2.s
@@ -19,12 +19,14 @@ for_i:
   bge end_for_i
   mov r6, #0 @ j = 0
   ldr r0, f__i
+  mov r1, r5
   bl printf
 for_j:
   cmp r6, r4 @ j < w2
   bge end_for_j
   mov r7, #0 @ k = 0
   ldr r0, f__j
+  mov r1, r6
   bl printf
 for_k:
   ldr r2, [sp, #4]
@@ -33,6 +35,7 @@ for_k:
 // Multiplication magic
   add r7, r7, #1 @ k++
   ldr r0, f__k
+  mov r1, r7
   bl printf
   b for_k
 end_for_k:
@@ -44,7 +47,6 @@ end_for_j:
 end_for_i:
   ldr lr, [sp, #16]@ load original lr into lr
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-  ldr lr, [sp, #16]@ load original lr into lr
 @ Stack: sp -> lr
   add sp, sp, #28 @ Clear the stack
 @ return
@@ -55,6 +57,6 @@ f__i:     .word formati
 f__j:     .word formatj
 f__k:     .word formatk
 .data
-formati:  .asciz "from assembly: i"
-formatj:  .asciz "from assembly: j"
-formatk:  .asciz "from assembly: k"
+formati:  .asciz "from assembly: i %d\n"
+formatj:  .asciz "from assembly: j %d\n"
+formatk:  .asciz "from assembly: k %d\n"
