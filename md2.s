3aab984692a490211966b2ee8fb13efd2fab0fba
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-07 01:10:07 +0200

    c

--- md2.s
+++ md2.s
@@ -3,15 +3,46 @@
 .global matmul
 .type matmul, %function
 
+@ str r1, [r5, #0] ---- stores r1 into *r5[0]
+
 @ int matmul(int h1, int w1, int *m1, int h2, int w2, int *m2, int *m3);
 matmul:
   stmfd sp!, {r0, r1, r2, r3, lr}
 @ sp -> r0, r1, r2, r3, lr
 @ # 0 = h1, #4 = w1, #8 = *m1, #12 = h2, #16 = LR, #20 = w2, #24 = *m2, #28 = *m3
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-  ldr r5, [sp, #28]@ load *m3 into r5
-  ldr r1, [sp, #0]
-  str r1, [r5, #0]@ store h1 into *m3
+  mov r5, #0 @ i = 0
+  mov r6, #0 @ j = 0
+  mov r7, #0 @ k = 0
+for_i:
+  cmp r5, r3 @ i < h2
+  bge end_for_i
+  mov r6, #0 @ j = 0
+  move r0 f__i
+  bl printf
+for_j:
+  cmp r6, r4 @ j < w2
+  bge end_for_j
+  mov r7, #0 @ k = 0
+  move r0 f__j
+  bl printf
+for_k:
+  ldr r2, [sp, #4]
+  cmp r7, r2 @ k < w1
+  bge end_for_k
+// Multiplication magic
+  add r7, r7, #1 @ k++
+  mov r0 f__k
+  bl printf
+  b for_k
+end_for_k:
+  add r6, r6, #1 @ j++
+  b for_j
+end_for_j:
+  add r5, r5, #1 @ i++
+  b for_i
+end_for_i:
+  ldr lr, [sp, #16]@ load original lr into lr
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   ldr lr, [sp, #16]@ load original lr into lr
 @ Stack: sp -> lr
@@ -20,8 +51,10 @@ matmul:
   bx lr
 @ h__a: .word height1
 @ w__a: .word width1
-f__1a:    .word format1
-f__2a:    .word format2
+f__i:     .word formati
+f__j:     .word formatj
+f__k:     .word formatk
 .data
-format1:  .asciz "from assembly H: %d\n"
-format2:  .asciz "from assembly W: %d\n"
+formati:  .asciz "from assembly: i"
+formatj:  .asciz "from assembly: j"
+formatk:  .asciz "from assembly: k"
