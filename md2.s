d3431bfa09270b8cbefa03d7e9e050c3ce423158
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-07 15:14:50 +0200

    c

--- md2.s
+++ md2.s
@@ -30,25 +30,23 @@ for_k:
   ldr r2, [sp, #12]@ load h2 into r2
   cmp r7, r2 @ k < h2
   bge end_for_k
-@ Multiplication happens here
+@@@@@ Multiplication happens here
+@ Get element from m1
   mov r10, r5, LSL#2 @ i * 4
-  mul r8, r10, r2 @ i * w1 * 4
+  mul r8, r10, r2 @ i * h2 * 4
   mov r9, r7, LSL#2
-  add r8, r8, r9 @ i * w1 * 4 + k * 4
+  add r8, r8, r9 @ i * h2 * 4 + k * 4
   ldr r0, [sp, #8] @ load *m1 into r0
   ldr r2, [r0, r8] @ load m1[i][k] into r2
-@ ldr r0, f__i
-@ mov r1, r2
-@ bl printf
+@ Get element from m2
   mul r8, r9, r4 @ j * 4 * w2
   mov r10, r6, LSL#2 @ j * 4
   add r8, r8, r10 @ j * 4 * w2 + k * 4
   ldr r1, [sp, #60] @ load *m2 into r1
   ldr r3, [r1, r8] @ load m2[k][j] into r3
-@ ldr r0, f__j
-@ mov r1, r3
-@ bl printf
-  mul r11, r2, r3 @ m1[i][k] * m2[k][j]
+@ Calculate their product
+  mul r11, r2, r3 @ r11 = m1[i][k] * m2[k][j]
+@ Put result into m3
   ldr r0, [sp, #64] @ load *m3 into r0
   mov r10, r5, LSL#2 @ i * 4
   mul r8, r10, r4 @ i * w2 * 4
@@ -56,7 +54,7 @@ for_k:
   add r8, r8, r12 @ i * w2 * 4 + j * 4
 
   ldr r1, [r0, r8] @ load m3[i][j] into r1
-  add r2, r11, r1 @ m3[i][j] += m1[i][k] * m2[k][j]
+  add r2, r11, r1  @ m3[i][j] += m1[i][k] * m2[k][j]
   str r2, [r0, r8] @ store m3[i][j] into *m3
 @ End of multiplication
   add r7, r7, #1 @ k++
@@ -72,12 +70,3 @@ end_for_i:
   ldmfd sp!, {r0-r12, lr}
 @ return
   bx lr
-@ h__a: .word height1
-@ w__a: .word width1
-f__i:     .word formati
-f__j:     .word formatj
-f__k:     .word formatk
-.data
-formati:  .asciz "from assembly: i %d\n"
-formatj:  .asciz "from assembly: j %d\n"
-formatk:  .asciz "from assembly: k %d\n"
