1fe5ce305893646471d11bb432dfb1edf00a691c
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-07 13:58:32 +0200

    c

--- md2.s
+++ md2.s
@@ -7,7 +7,7 @@
 
 @ int matmul(int h1, int w1, int *m1, int h2, int w2, int *m2, int *m3);
 matmul:
-  stmfd sp!, {r0, r1, r2, r3, lr}
+  stmfd sp!, {r0-lr}
 @ sp -> r0, r1, r2, r3, lr
 @ # 0 = h1, #4 = w1, #8 = *m1, #12 = h2, #16 = LR, #20 = w2, #24 = *m2, #28 = *m3
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -15,27 +15,27 @@ matmul:
   mov r6, #0 @ j = 0
   mov r7, #0 @ k = 0
 for_i:
-  ldr r3, [sp, #12] @ load h2 into r3
+  ldr r3, [sp, #12]@ load h2 into r3
   cmp r5, r3 @ i < h2
   bge end_for_i
   mov r6, #0 @ j = 0
 for_j:
-  ldr r4, [sp, #20] @ load w2 into r4
+  ldr r4, [sp, #20]@ load w2 into r4
   cmp r6, r4 @ j < w2
   bge end_for_j
   mov r7, #0 @ k = 0
 for_k:
-  ldr r2, [sp, #4] @ load w1 into r2
+  ldr r2, [sp, #4]@ load w1 into r2
   cmp r7, r2 @ k < w1
   bge end_for_k
 @ Multiplication happens here
   ldr r0, [sp, #8] @ load *m1 into r0
   ldr r1, [sp, #24] @ load *m2 into r1
-  stmfd sp!, {r5, r6, r7}
+  stmfd sp!, {r6}
+  mul r6, r5, r2 @ i * w1
   ldr r2, [r0, #0] @ load m1[i][k] into r2
   ldr r3, [r1, #0] @ load m2[k][j] into r3
   mul r4, r2, r3 @ m1[i][k] * m2[k][j]
-  ldmfd sp!, {r5, r6, r7}
   ldr r0, [sp, #28] @ load *m3 into r0
   ldr r1, [r0, #0] @ load m3[i][j] into r1
   add r2, r4, r1 @ m3[i][j] += m1[i][k] * m2[k][j]
